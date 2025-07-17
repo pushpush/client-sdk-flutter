290c866eda39b1f869a32a11a5267b9c63d19987
pushpush <pushpush@users.noreply.github.com>
2025-07-17 22:10:26 +0300

    feat: chat message

--- lib/src/core/engine.dart
+++ lib/src/core/engine.dart
@@ -981,7 +981,7 @@ class Engine extends Disposable with EventsEmittable<EngineEvent> {
         ),
       );
     } else if (dp.whichValue() == lk_models.DataPacket_Value.chatMessage) {
-      // Data Stream trailer
+      // Chat Message
       events.emit(
         EngineChatMessageEvent(
           chatMessage: dp.chatMessage,
